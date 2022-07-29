0a9cbf419ec27693c87090d1f8d3e166d3a64b7b
Loïc Chalmandrier <loic_chr@mailoo.org>
2022-07-29 16:59:13 +0200

    findLVeq corrections

--- R/findLVeq.R
+++ R/findLVeq.R
@@ -6,14 +6,14 @@ findLVeq <- function(K, A){
   if(any(Ns < 0)){
     excluded <- c(excluded, which.min(Ns))
     Ks_red <- K[-excluded]
-    alphas <- A[-excluded, -excluded]
+    alphas <- A[-excluded, -excluded, drop = F]
     Ns <- K*0
     Ns[-excluded] <- pseudoinverse(alphas) %*% as.matrix(Ks_red)
 
     while(any(Ns < 0)){
       excluded <- c(excluded, which.min(Ns))
       Ks_red <- K[-excluded]
-      alphas <- as.matrix(A[-excluded, -excluded])
+      alphas <- as.matrix(A[-excluded, -excluded, drop = F])
       Ns <- K*0
       Ns[-excluded] <- pseudoinverse(alphas) %*% as.matrix(Ks_red)
     }
